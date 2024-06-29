60e076c0a0b737c6128faffa0e1ff6e22433fcb1
lolovski <alexpaxyaev@gmail.com>
2024-06-29 15:47:04 +0300

    base step 1

--- core/database/models.py
+++ core/database/models.py
@@ -1,11 +1,15 @@
-from sqlalchemy import String, BigInteger, ForeignKey
+from sqlalchemy import String, BigInteger, ForeignKey, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker, relationship
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+import os
+from dotenv import load_dotenv
+load_dotenv()
+postgres_engine = create_async_engine(str(os.getenv('DATABASE_URL')))
+async_postgres_session = async_sessionmaker(postgres_engine)
 
-engine = create_async_engine('sqlite+aiosqlite:///db.sqlite3')
-
-async_session = async_sessionmaker(engine)
-
+"""sqlite_engine = create_async_engine('sqlite+aiosqlite:///db.sqlite3')
+async_sqlite_session = async_sessionmaker(sqlite_engine)
+sqlite_session = async_postgres_session()"""
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -13,13 +17,13 @@ class Base(AsyncAttrs, DeclarativeBase):
 
 class BodyPart(Base):
     __tablename__ = 'body_parts'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     name: Mapped[str] = mapped_column(String(32))
 
 
 class Exercise(Base):
     __tablename__ = 'exercises'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     name: Mapped[str] = mapped_column(String(64))
     body_part: Mapped[int] = mapped_column(ForeignKey('body_parts.id'))
     text: Mapped[str] = mapped_column(String(4096))
@@ -34,7 +38,7 @@ class Exercise(Base):
 
 class Photo(Base):
         __tablename__ = 'photos'
-        photo_id: Mapped[int] = mapped_column(primary_key=True)
+        photo_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
         file_path: Mapped[str] = mapped_column(String(1024))
         exercise_id: Mapped[int] = mapped_column(ForeignKey('exercises.id'))
         paragraph: Mapped[int] = mapped_column(nullable=True)
@@ -42,14 +46,14 @@ class Photo(Base):
 
 class Training(Base):
     __tablename__ = 'trainings'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     name: Mapped[str] = mapped_column(String(128))
     text: Mapped[str] = mapped_column(String(2048))
 
 
 class TrainingDay(Base):
     __tablename__ = 'training_days'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     training_id: Mapped[int] = mapped_column(ForeignKey('trainings.id'))
     number: Mapped[int] = mapped_column()
     exercise_replied: Mapped[list['Exercise']] = relationship(
@@ -64,8 +68,30 @@ class DayExercises(Base):
     exercise_id: Mapped[int] = mapped_column(ForeignKey('exercises.id', ondelete='CASCADE'), primary_key=True)
 
 
-
-
 async def async_main():
-    async with engine.begin() as conn:
+    async with postgres_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+"""async def add_table_db(list_new_bp):
+    async with async_postgres_session() as session:
+        for new_body_part in list_new_bp:
+            session.add(new_body_part)
+            await session.commit()
+            print(new_body_part)
+
+
+async def dump_db():
+    list_new_bp = []
+    async with async_sqlite_session() as session:
+        body_parts = await session.scalars(select(Training))
+        for body_part in body_parts:
+            new_body_part = Training(
+
+            )
+            list_new_bp.append(new_body_part)
+        print(list_new_bp)
+        return list_new_bp
+"""
+
+
--- core/database/requests.py
+++ core/database/requests.py
@@ -1,4 +1,4 @@
-from core.database.models import BodyPart, async_session, Exercise, Photo
+from core.database.models import BodyPart, async_postgres_session, Exercise, Photo
 from sqlalchemy import select, update, insert, delete
 
 
@@ -14,46 +14,47 @@ from sqlalchemy import select, update, insert, delete
 
 
 async def get_body_parts_list():
-    async with async_session() as session:
+    async with async_postgres_session() as session:
         body_parts = await session.scalars(select(BodyPart))
         return body_parts
 
 
 async def get_exercises_list(body_part_id):
-    async with async_session() as session:
-        exercises = await session.scalars(select(Exercise).where(Exercise.body_part == body_part_id))
+    async with async_postgres_session() as session:
+
+        exercises = await session.scalars(select(Exercise).where(Exercise.body_part == int(body_part_id)))
         return exercises
 
 
 async def get_this_exercise(exercise_id):
-    async with async_session() as session:
-        exercise = await session.scalar(select(Exercise).where(Exercise.id == exercise_id))
+    async with async_postgres_session() as session:
+        exercise = await session.scalar(select(Exercise).where(Exercise.id == int(exercise_id)))
         return exercise
 
 
 async def set_photo(file_path, exercise_id, paragraph):
-    async with async_session() as session:
+    async with async_postgres_session() as session:
         session.add(Photo(file_path=file_path, exercise_id=exercise_id, paragraph=paragraph))
         await session.commit()
         return Photo(file_path=file_path, exercise_id=exercise_id, paragraph=paragraph)
 
 
 async def get_photos(exercise_id):
-    async with async_session() as session:
-        photos = await session.scalars(select(Photo).where(Photo.exercise_id == exercise_id))
+    async with async_postgres_session() as session:
+        photos = await session.scalars(select(Photo).where(Photo.exercise_id == int(exercise_id)))
         if photos:
             return photos
         else: return None
 
 
 async def set_exercise(body_part, name, text, title):
-    async with async_session() as session:
+    async with async_postgres_session() as session:
         session.add(Exercise(body_part=body_part, name=name, text=text, title=title))
         await session.commit()
         return Exercise(body_part=body_part, name=name, text=text, title=title)
 
 
 async def get_last_exercise():
-    async with async_session() as session:
+    async with async_postgres_session() as session:
         last_exercise = await session.scalars(select(Exercise))
         return last_exercise.all()[-1]
